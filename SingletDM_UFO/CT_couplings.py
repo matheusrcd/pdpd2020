1a3e6ff55ea6b0f2feb495532dc4e064aec5ec24
Andre Lessa <lessa.a.p@gmail.com>
2021-03-29 15:01:25 -0300

    Added model

---
+++ SingletDM_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.43
+# Mathematica version: 12.1.0 for Linux x86 (64-bit) (March 18, 2020)
+# Date: Mon 29 Mar 2021 14:58:25
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ SingletDM_UFO/coupling_orders.py
@@ -0,0 +1,28 @@
+# This file was automatically created by FeynRules 2.3.43
+# Mathematica version: 12.1.0 for Linux x86 (64-bit) (March 18, 2020)
+# Date: Mon 29 Mar 2021 14:58:25
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NPS = CouplingOrder(name = 'NPS',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+NPT = CouplingOrder(name = 'NPT',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
